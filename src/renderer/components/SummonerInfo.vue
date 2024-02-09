5c45d7ffd18be38d50fdce31f86b472650841b5a
markus.romaniw@sap.com <markus.romaniw@sap.com>
2024-02-09 19:27:11 +0100

    summonerTable fetching if not in cache

--- src/renderer/components/SummonerInfo.vue
+++ src/renderer/components/SummonerInfo.vue
@@ -1,178 +1,200 @@
 <template>
-	<div class="dropdown summoner-display" ref="dropdownContainer">
-		<!-- Dropdown button -->
-		<button class="btn btn-dark dropdown-toggle position-relative" type="button" id="dropdownSummonerButton"
-			@click="toggleDropdown" :aria-expanded="dropdownOpen.toString()">
-			<div class="d-flex align-items-center">
-				<div class="icon-container">
-					<img :src="getSummonerIcon(profileIconId)" alt="Summoner Icon" class="rounded-circle icon-image">
-					<span class="level-pill">{{ level }}</span>
-				</div>
-				<span class="ps-2 text-light">{{ currentSummonerName }}</span>
-				<span class="text-secondary">#{{ riotIdParts.tagLine }}</span>
-			</div>
-		</button>
-		<!-- Dropdown Menu -->
-		<div class="dropdown-menu" :class="{ 'show': dropdownOpen }" aria-labelledby="dropdownSummonerButton">
-			<div class="dropdown-header">Account</div>
-			<div v-for="detail in allPlayerDetails" :key="detail.summonerData.name" class="dropdown-item position-relative"
-				@click="selectSummoner(detail.summonerData.name)">
-				<img :src="getSummonerIcon(detail.summonerData.profileIconId)" alt="Summoner Icon" class="icon-menu-image">
-				<span class="ms-2">{{ detail.summonerData.name }}</span>
-			</div>
-		</div>
-	</div>
+  <div class="dropdown summoner-display" ref="dropdownContainer">
+    <button
+      class="btn btn-dark dropdown-toggle position-relative"
+      type="button"
+      @click="dropdownOpen = !dropdownOpen"
+      :aria-expanded="dropdownOpen.toString()"
+    >
+      <div class="d-flex align-items-center">
+        <div class="icon-container">
+          <!-- Adjust src and alt as necessary based on the current selection -->
+          <img
+            :src="currentSelection ? getSummonerIcon(currentSelection.profileiconid) : defaultIcon"
+            alt="Summoner Icon"
+            class="rounded-circle icon-image"
+          >
+          <span
+            class="level-pill"
+          >{{ currentSelection ? currentSelection.summonerlevel : 'N/A' }}</span>
+        </div>
+        <span class="ps-2 text-light">{{ currentSelection.gamename || 'Select Summoner' }}</span>
+        <span class="text-secondary">#{{ currentSelection.tagline || '' }}</span>
+      </div>
+    </button>
+    <div class="dropdown-menu" :class="{ 'show': dropdownOpen }">
+      <div class="dropdown-header">Account</div>
+      <div
+        v-for="detail in allPlayerDetails"
+        :key="detail.puuid"
+        class="dropdown-item position-relative"
+        @click="selectSummoner(detail)"
+      >
+        <img
+          :src="getSummonerIcon(detail.profileIconId)"
+          alt="Summoner Icon"
+          class="icon-menu-image"
+        >
+        <span class="ms-2">{{ detail.name }}</span>
+      </div>
+    </div>
+  </div>
 </template>
 
+
 <script setup>
-import { ref, computed, onMounted, onBeforeUnmount } from 'vue';
-import { useStore } from 'vuex';
-import { getUrlHelper } from '../globalSetup'; // Adjust path as needed
+import {ref, computed, onMounted, onBeforeUnmount} from 'vue';
+import {useStore} from 'vuex';
+import {getUrlHelper} from '../globalSetup'; // Adjust path as needed
+const store=useStore();
 
-const dropdownContainer = ref(null);
+const dropdownContainer=ref(null);
+const dropdownOpen=ref(false);
 
-const store = useStore();
-const dropdownOpen = ref(false);
-const profileIconId = computed(() => store.getters['summoner/profileIconId']);
+const profileIconId=computed(() => store.getters['summoner/profileIconId']);
+const currentSelection=ref({});
 
-const currentSummonerName = computed(() => store.getters['summoner/currentSummonerName']);
-const allPlayerDetails = computed(() => store.getters['summoner/getAllPlayerDetails']);
+const allPlayerDetails=computed(() => store.getters['summoner/getAllPlayerDetails']);
 // Somewhere in your Vue component
 
-const getSummonerIcon = (iconId) => {
-	const urlHelper = getUrlHelper();
-	return urlHelper.getSummonerIconUrl(iconId);
+const getSummonerIcon=(iconId) => {
+  const urlHelper=getUrlHelper();
+  return urlHelper.getSummonerIconUrl(iconId);
 };
 
-const summonerNames = store.getters['summoner/allSummonerNames'];
-const riotIdParts = store.getters['summoner/riotIdParts'];
-const level = store.getters['summoner/level'];
-
 function toggleDropdown() {
-	dropdownOpen.value = !dropdownOpen.value;
+  dropdownOpen.value=!dropdownOpen.value;
 }
 
 function handleClickOutside(event) {
-	if (dropdownContainer.value && !dropdownContainer.value.contains(event.target)) {
-		dropdownOpen.value = false;
-	}
+  if (dropdownContainer.value&&!dropdownContainer.value.contains(event.target)) {
+    dropdownOpen.value=false;
+  }
 }
-function selectSummoner(summoner) {
-	store.dispatch('summoner/setCurrentSummoner', summoner);
-	dropdownOpen.value = false;
+function selectSummoner(summonerDetail) {
+  currentSelection.value=summonerDetail;
+  dropdownOpen.value=false; // Close the dropdown after selection
 }
 
 onMounted(() => {
-	document.addEventListener('click', handleClickOutside);
+  document.addEventListener('click', handleClickOutside);
+  if (!allPlayerDetails.value.length) {
+    store.dispatch('summoner/fetchSummonerDataByAccountId').then(() => {
+      // Default to the first entry after data is fetched
+      if (allPlayerDetails.value.length) {
+        currentSelection.value=allPlayerDetails.value[0];
+      }
+    });
+  } else {
+    // If player details are already loaded, default to the first entry
+    currentSelection.value=allPlayerDetails.value[0];
+  }
 });
 
 onBeforeUnmount(() => {
-	document.removeEventListener('click', handleClickOutside);
+  document.removeEventListener('click', handleClickOutside);
 });
 </script>
   
 <style scoped>
 .summoner-display {
-	border: none;
-	margin: 1rem 0;
-	align-items: center;
-	cursor: pointer;
-	display: inline-block;
-	position: relative;
+  border: none;
+  margin: 1rem 0;
+  align-items: center;
+  cursor: pointer;
+  display: inline-block;
+  position: relative;
 }
 
 #dropdownSummonerButton {
-	background: transparent;
-	border: none;
-	padding: 1rem;
-	border-radius: 6px 6px 0px 0px;
-
+  background: transparent;
+  border: none;
+  padding: 1rem;
+  border-radius: 6px 6px 0px 0px;
 }
 
 #dropdownSummonerButton:hover {
-	background: var(--grey-4);
-	border-bottom: 0px;
-	transition: background 0.2s ease-in-out;
+  background: var(--grey-4);
+  border-bottom: 0px;
+  transition: background 0.2s ease-in-out;
 }
 
 #dropdownSummonerButton:focus {
-	background: var(--grey-4);
-	border-bottom: 0px;
-	transition: background 0.2s ease-in-out;
+  background: var(--grey-4);
+  border-bottom: 0px;
+  transition: background 0.2s ease-in-out;
 }
 
 .dropdown-menu.show {
-	display: block;
+  display: block;
 }
 
 .icon-container {
-	position: relative;
+  position: relative;
 }
 
 .icon-image {
-	width: 45px;
-	height: 45px;
-	border: 2px solid #c0c0c0;
+  width: 45px;
+  height: 45px;
+  border: 2px solid #c0c0c0;
 }
 
 .icon-menu-image {
-	width: 30px;
-	height: 30px;
-	border-radius: 50%;
-	border: 1px solid #c0c0c0;
+  width: 30px;
+  height: 30px;
+  border-radius: 50%;
+  border: 1px solid #c0c0c0;
 }
 
 .level-pill {
-	position: absolute;
-	bottom: -5px;
-	left: 5px;
-	background-color: #000000;
-	color: #d3d3d3;
-	padding: 2px 6px;
-	border-radius: 10px;
-	font-size: 0.65rem;
-	font-weight: bold;
+  position: absolute;
+  bottom: -5px;
+  left: 5px;
+  background-color: #000000;
+  color: #d3d3d3;
+  padding: 2px 6px;
+  border-radius: 10px;
+  font-size: 0.65rem;
+  font-weight: bold;
 }
-
 
 .dropdown-menu {
-	background: var(--grey-4);
-	color: #f8f9fa;
-	border-radius: 0 0 6px 6px;
-	border-top: none;
-	width: 100%;
-	box-shadow: 0 3px 0 rgba(0, 0, 0, 0.25);
+  background: var(--grey-4);
+  color: #f8f9fa;
+  border-radius: 0 0 6px 6px;
+  border-top: none;
+  width: 100%;
+  box-shadow: 0 3px 0 rgba(0, 0, 0, 0.25);
 }
-
 
 /* Dropdown item styling */
 .dropdown-item {
-	background: none;
-	cursor: pointer;
-	border-radius: 0.25rem;
-	box-shadow: 0 1px 0 rgba(0, 0, 0, 0.05);
+  background: none;
+  cursor: pointer;
+  border-radius: 0.25rem;
+  box-shadow: 0 1px 0 rgba(0, 0, 0, 0.05);
 }
 
 .dropdown-item .flex-grow-1 {
-	display: flex;
-	flex-direction: column;
+  display: flex;
+  flex-direction: column;
 }
 
 .dropdown-item,
 .dropdown-header {
-	font-size: 1rem;
-	color: inherit;
+  font-size: 1rem;
+  color: inherit;
 }
 
 .dropdown-header {
-	font-size: 0.85rem;
-	text-transform: uppercase;
+  font-size: 0.85rem;
+  text-transform: uppercase;
 }
 
 .dropdown-item:hover,
 .dropdown-item:focus {
-	background: rgba(45, 57, 65, 0.395);
-	color: inherit;
+  background: rgba(45, 57, 65, 0.395);
+  color: inherit;
 }
 </style>
   
