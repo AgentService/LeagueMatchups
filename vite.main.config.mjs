2670456a727bff9690f08c6a4cd37c8baec2640a
AgentService <markusromaniw@gmx.de>
2024-03-16 00:49:29 +0100

    package vite config

--- vite.main.config.mjs
+++ vite.main.config.mjs
@@ -1,33 +1,23 @@
 //vite.main.config.mjs
 
-import { defineConfig } from "vite";
-import path from "path";
+import { defineConfig } from 'vite';
 
 export default defineConfig({
-  mode: "production",
-  build: {
-    outDir: path.resolve(__dirname, "./dist"), // Ensure this points to the correct output directory
-    emptyOutDir: true, // Caution: This option will remove all files in the outDir when the build starts
-    rollupOptions: {
-      input: path.resolve(__dirname, "src/main/index.js"), // Adjust if your entry file is located elsewhere
-      output: {
-        format: "cjs",
-        entryFileNames: "main.js",
-        dir: path.resolve(__dirname, "dist"),
-      },
-    },
-    rollupOptions: {
-      external: ["node-fetch"],
-    },
-  },
   server: {
     proxy: {
-      "/api": "http://localhost:3001",
+      '/api': 'http://localhost:3001',
     },
+  },
+  build: {
+    rollupOptions: {
+      external: ['node-fetch']
+    },
+  
   },
   resolve: {
     alias: {
       // other aliases
     },
-  },
+  }
 });
+
