6aefe8b01597dfda75b7e88c958eeeafe8d26762
AgentService <markusromaniw@gmx.de>
2024-11-07 02:45:51 +0100

    releasesh

--- release.sh
+++ release.sh
@@ -5,14 +5,14 @@ if [ -z "$1" ]; then
   echo "Usage: ./release.sh [patch|minor|major]"
   exit 1
 fi
-git add .
-git commit -m "Release version $(node -p "require('./package.json').version")"
-git tag -a v$(node -p "require('./package.json').version") -m "Release version $(node -p "require('./package.json').version")"
+
 # Step 1: Update the version
 npm version $1 || exit 1
 
 # Step 2: Commit and tag the version
 git add .
+git commit -m "Release version $(node -p "require('./package.json').version")"
+git tag -a v$(node -p "require('./package.json').version") -m "Release version $(node -p "require('./package.json').version")"
 
 # Step 3: Push commits and tags to origin
 git push origin --follow-tags || exit 1
